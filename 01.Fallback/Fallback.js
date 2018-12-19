8ca394d81b2067affbe9b7f38592c0933af63243
NotSurprised <roro8016489@gmail.com>
2018-12-20 01:03:33 +0800

    20181220

---
+++ 01.Fallback/Fallback.js
@@ -0,0 +1,5 @@
+contract.contribute({value: 1})
+contract.send(1)
+//contract.sendTransaction({value: 1}); whatever func not in contract but Web3.js
+contract.owner(); // check is own switch?
+contract.withdraw(); // give me money.
