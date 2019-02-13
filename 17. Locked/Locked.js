bb8265c993eb7e325dbe07b02f407e951854cdb0
NotSurprised <roro8016489@gmail.com>
2019-02-13 14:04:44 +0800

    20190213

---
+++ 17. Locked/Locked.js
@@ -0,0 +1,3 @@
+await contract.unlocked()
+await contract.register("0x0000000000000000000000000000000000000000000000000000000000000001",0xeBa......) // any address, it doesn't matter.
+await contract.unlocked()
