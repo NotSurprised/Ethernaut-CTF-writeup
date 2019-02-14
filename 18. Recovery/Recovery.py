2fb09d850a3277721f3544f8baac65a9f6f1d2fa
NotSurprised <roro8016489@gmail.com>
2019-02-15 01:48:33 +0800

    20190215

---
+++ 18. Recovery/Recovery.py
@@ -0,0 +1,26 @@
+def rlp_encode(input):
+    if isinstance(input,str):
+        if len(input) == 1 and ord(input) < 0x80: return input
+        else: return encode_length(len(input), 0x80) + input
+    elif isinstance(input,list):
+        output = ''
+        for item in input: output += rlp_encode(item)
+        return encode_length(len(output), 0xc0) + output
+
+def encode_length(L,offset):
+    if L < 56:
+         return chr(L + offset)
+    elif L < 256**8:
+         BL = to_binary(L)
+         return chr(len(BL) + offset + 55) + BL
+    else:
+         raise Exception("input too long")
+
+def to_binary(x):
+    if x == 0:
+        return ''
+    else: 
+        return to_binary(int(x / 256)) + chr(x % 256)
+
+print rlp_encode(["e2acb72b2cb49b3661f9e7577d743a6b3a853933".decode('hex'),"03".decode('hex')]).encode('hex')
+
